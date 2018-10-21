456b2e8c7013f49a7f624dfe4c1a7be80a267cea
Lauren <lmcdono17@gmail.com>
2018-10-21 02:09:38 -0400

    more changes

--- server.js
+++ server.js
@@ -13,23 +13,30 @@ $(document).ready(function() {
 
 
 
-// Instantiate the Bootstrap carousel
-$('.multi-item-carousel').carousel({
-    interval: false
-  });
+(function(){
+    // setup your carousels as you normally would using JS
+    // or via data attributes according to the documentation
+    // https://getbootstrap.com/javascript/#carousel
+    $('#carousel123').carousel({ interval: 2000 });
+    $('#carouselABC').carousel({ interval: 3600 });
+  }());
   
-  // for every slide in carousel, copy the next slide's item in the slide.
-  // Do the same for the next, next item.
-  $('.multi-item-carousel .item').each(function(){
-    var next = $(this).next();
-    if (!next.length) {
-      next = $(this).siblings(':first');
-    }
-    next.children(':first-child').clone().appendTo($(this));
-    
-    if (next.next().length>0) {
-      next.next().children(':first-child').clone().appendTo($(this));
-    } else {
-        $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-    }
-  });
+  (function(){
+    $('.carousel-showmanymoveone .item').each(function(){
+      var itemToClone = $(this);
+  
+      for (var i=1;i<4;i++) {
+        itemToClone = itemToClone.next();
+  
+        // wrap around if at end of item collection
+        if (!itemToClone.length) {
+          itemToClone = $(this).siblings(':first');
+        }
+  
+        // grab item, clone, add marker class, add to collection
+        itemToClone.children(':first-child').clone()
+          .addClass("cloneditem-"+(i))
+          .appendTo($(this));
+      }
+    });
+  }());
